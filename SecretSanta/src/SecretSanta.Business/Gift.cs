4ac8d17170fcf3b2111f3f56b50322cee372055c
Zuelke <szuelke@ewu.edu>
2020-01-09 13:23:20 -0800

    Built classes and Nullable

---
+++ SecretSanta/src/SecretSanta.Business/Gift.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecretSanta.Business
+{
+    class Gift
+    {
+        int Id { get; }
+        string Title;
+        string Description;
+        string Url;
+        User User;
+
+        public Gift(int id, string title, string description, string url, User user)
+        {
+            this.Id = id;
+            this.Title = title;
+            this.Description = description;
+            this.Url = url;
+            this.User = user;
+        }
+    }
+}
---
+++ SecretSanta/src/SecretSanta.Business/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecretSanta.Business
+{
+    class User
+    {
+        int Id { get; }
+        string FirstName;
+        string LastName;
+        IList<Gift> Gifts = new List<Gift>();
+
+        public User(int id, string fname, string lname)
+        {
+            this.Id = id;
+            this.FirstName = fname;
+            this.LastName = lname;
+        }
+    }
+}
