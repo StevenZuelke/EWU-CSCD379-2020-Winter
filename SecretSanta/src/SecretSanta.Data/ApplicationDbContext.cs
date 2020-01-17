e4a2f1e29c4a6623a6bc16a43409b0520652f8e7
Steven Zuelke <stevezuelkepsn@gmail.com>
2020-01-16 22:09:31 -0800

    HW2 Update

--- SecretSanta/src/SecretSanta.Data/ApplicationDbContext.cs
+++ SecretSanta/src/SecretSanta.Data/ApplicationDbContext.cs
@@ -1,10 +1,25 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace SecretSanta.Data
 {
-    class ApplicationDbContext
+    public class ApplicationDbContext:DbContext
     {
+        //DbSets for user, gift, and group
+        public DbSet<User> Users { get; set; }
+        public DbSet<Gift> Gifts { get; set; }
+        public DbSet<Group> Groups { get; set; }
+        //Member variable for IHttpContextAccessor
+        public IHttpContextAccessor HttpContextAccessor { get; set; }
+        //Constructor with only options
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        //Constructor with both options and httpAccessor
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IHttpContextAccessor httpContextAccessor)
+        {
+            HttpContextAccessor = httpContextAccessor;
+        }
     }
 }
