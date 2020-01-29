2acb0d5ada944ef86f5823431b70f15886a1532a
Steven Zuelke <stevezuelkepsn@gmail.com>
2020-01-28 19:19:28 -0800

    Up to Controllers

--- SecretSanta/src/SecretSanta.Api/Startup.cs
+++ SecretSanta/src/SecretSanta.Api/Startup.cs
@@ -1,8 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-
+using SecretSanta.Business;
+using SecretSanta.Business.Services;
+using SecretSanta.Data;
+using AutoMapper;
 namespace SecretSanta.Api
 {
     // Justification: Disable until ConfigureServices is added back.
@@ -12,9 +16,18 @@ namespace SecretSanta.Api
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        //public void ConfigureServices(IServiceCollection services)
-        //{
-        //}
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //Setup Dependency Injection and Register services
+            services.AddScoped<IGiftService, GiftService>();
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IGroupService, GroupService>();
+            services.AddDbContext<ApplicationDbContext>();
+
+            //NSwagger
+            services.AddMvc();
+            services.AddSwaggerDocument();
+        }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public static void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -23,8 +36,13 @@ namespace SecretSanta.Api
             {
                 app.UseDeveloperExceptionPage();
             }
+            //NSwagger
+            app.UseOpenApi();
+            app.UseSwaggerUi3();
+            
 
             app.UseRouting();
+           
 
             app.UseEndpoints(endpoints =>
             {
