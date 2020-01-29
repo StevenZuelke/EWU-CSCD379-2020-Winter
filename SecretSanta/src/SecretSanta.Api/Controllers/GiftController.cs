e907576fd767d1884c2c01e8b85578abe4fc5953
Steven Zuelke <stevezuelkepsn@gmail.com>
2020-01-29 00:10:20 -0800

    Finished Controllers. Need Tests

---
+++ SecretSanta/src/SecretSanta.Api/Controllers/GiftController.cs
@@ -0,0 +1,78 @@
+﻿using SecretSanta.Business;
+using SecretSanta.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SecretSanta.Api.Controllers
+{
+    //https://localhost/api/Gi
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GiftController : ControllerBase
+    {
+        private IGiftService GiftService { get; }
+
+        public GiftController(IGiftService giftService)
+        {
+            GiftService = giftService ?? throw new System.ArgumentNullException(nameof(giftService));
+        }
+
+        // GET: https://localhost/api/Gift
+        [HttpGet]
+        public async Task<IEnumerable<Gift>> Get()
+        {
+            List<Gift> gifts = await GiftService.FetchAllAsync();
+            return gifts;
+        }
+
+        // GET: api/Gift/5
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<Gift>> Get(int id)
+        {
+            if (await GiftService.FetchByIdAsync(id) is Gift gift)
+            {
+                return Ok(gift);
+            }
+            return NotFound();
+        }
+
+        // POST: api/Gift
+        [HttpPost]
+        public async Task<Gift> Post([FromBody] Gift value)
+        {
+            return await GiftService.InsertAsync(value);
+        }
+
+        // PUT: api/Gift/5
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult<Gift>> Put(int id, [FromBody] Gift value)
+        {
+            if (await GiftService.FetchByIdAsync(id) is Gift gift)
+            {
+                return Ok(GiftService.UpdateAsync(id, value));
+            }
+            return NotFound();
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult<Gift>> Delete(int id)
+        {
+            if (await GiftService.FetchByIdAsync(id) is Gift gift)
+            {
+                return Ok(GiftService.DeleteAsync(id));
+            }
+            return NotFound();
+        }
+    }
+}
---
+++ SecretSanta/src/SecretSanta.Api/Controllers/UserController.cs
@@ -0,0 +1,78 @@
+﻿using SecretSanta.Business;
+using SecretSanta.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SecretSanta.Api.Controllers
+{
+    //https://localhost/api/Gi
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private IUserService UserService { get; }
+
+        public UserController(IUserService userService)
+        {
+            UserService = userService ?? throw new System.ArgumentNullException(nameof(userService));
+        }
+
+        // GET: https://localhost/api/User
+        [HttpGet]
+        public async Task<IEnumerable<User>> Get()
+        {
+            List<User> users = await UserService.FetchAllAsync();
+            return users;
+        }
+
+        // GET: api/User/5
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<User>> Get(int id)
+        {
+            if (await UserService.FetchByIdAsync(id) is User user)
+            {
+                return Ok(user);
+            }
+            return NotFound();
+        }
+
+        // POST: api/User
+        [HttpPost]
+        public async Task<User> Post([FromBody] User value)
+        {
+            return await UserService.InsertAsync(value);
+        }
+
+        // PUT: api/User/5
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult<User>> Put(int id, [FromBody] User value)
+        {
+            if (await UserService.FetchByIdAsync(id) is User user)
+            {
+                return Ok(UserService.UpdateAsync(id, value));
+            }
+            return NotFound();
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult<User>> Delete(int id)
+        {
+            if (await UserService.FetchByIdAsync(id) is User user)
+            {
+                return Ok(UserService.DeleteAsync(id));
+            }
+            return NotFound();
+        }
+    }
+}
